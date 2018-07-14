8589fc667ebce30af357e1e4df36544b8030aaf4
WEI_SYUAN SYU <wei_syuan@itri.org.tw>
2018-07-14 10:42:33 +0800

    四種Controller傳Data到View的方法 1.ViewData Model 2.ViewBag 3.ViewData 4.TempData

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -6,12 +6,40 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class MBController : Controller
+    public class MBController : BaseController
     {
         // GET: MB
         public ActionResult Index()
         {
+            var data = "Hello World";
+            ViewData.Model = data;  //強型別
             return View();
         }
+
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.Text = "ViewBag";
+            return View();
+        }
+        public ActionResult ViewDataDemo()
+        {
+            ViewData["Text"] = "ViewData";
+            return View();
+        }
+
+
+        #region 通常表單會用這種技巧 寫入成功使用這在重整是不會再做第二次了
+        public ActionResult TempDataDemo()
+        {
+            return View();
+        }
+
+
+        public ActionResult SaveTempData()
+        {
+            TempData["Text"] = "TempData";
+            return Redirect("TempDataDemo");
+        }
+        #endregion
     }
 }
